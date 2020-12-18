d656525599dc802fb688d91abec8d40e9bb7abe4
JoaquinAbenza <modelos@notin.net>
2020-12-18 19:59:56 +0100

    tslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,31 @@
 module.exports = {
-    "extends": "tui/es6",
-    "env": {
-        "browser": true,
-        "amd": true,
-        "node": true,
-        "jasmine": true,
-        "jquery": true,
-        "es6": true
+    'extends': 'tui/es6',
+    'env': {
+        'browser': true,
+        'amd': true,
+        'node': true,
+        'jasmine': true,
+        'jquery': true,
+        'es6': true
     },
-    "globals": {
-        "fabric": true,
-        "tui": true,
-        "loadFixtures": true
+    'globals': {
+        'fabric': true,
+        'tui': true,
+        'loadFixtures': true
     },
-    "parserOptions": {
-        "sourceType": "module"
+    'parserOptions': {
+        'sourceType': 'module'
     },
     'rules': {
-        indent: [2, 4, {SwitchCase: 1, ignoreComments: false, ImportDeclaration: 1, flatTernaryExpressions: false}],
+        indent: [2, 4, {SwitchCase: 1,
+            ignoreComments: false,
+            ImportDeclaration: 1,
+            flatTernaryExpressions: false}],
         'prefer-destructuring': ['error', {
-            VariableDeclarator: {array: true, object: true},
-            AssignmentExpression: {array: false, object: false}
+            VariableDeclarator: {array: true,
+                object: true},
+            AssignmentExpression: {array: false,
+                object: false}
         }]
     }
 };
